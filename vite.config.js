8d106f962d093d1f65f2f49984e50994500ea54b
Bárbara Quiroz <b.quirozs27@gmail.com>
2026-04-12 23:59:14 -0400

    1.1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +12,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    githubPagesSpa({
+      // Options are optional
+      // verbose: true, // Set to false to disable console logs
+    }),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
